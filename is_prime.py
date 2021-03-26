20f42c35b86057f57cb899866cc82f2d5aa395c6
abdelilah <abdelilah@gmail.com>
2021-03-26 12:58:09 +0100

    check prime numbers

---
+++ is_prime.py
@@ -0,0 +1,9 @@
+while 1 :
+    number = int(input("\nGive a number to check out : "))
+    for i in range(2,(number // 2)+1 ):
+        if number % i == 0 :
+            j = number / i
+            print( "\n" + str(number) + " = " + str(i) + " * " + str(j) )
+            break
+    else :
+        print( "\n" + str(number) + " is a prime number! ")
